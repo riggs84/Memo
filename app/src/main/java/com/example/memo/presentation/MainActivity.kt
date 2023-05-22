cc9ef780eee1fce269c5c86b627dcc39db13ea8b
viktoriurkov <victor.iurkov@gmail.com>
2023-05-22 23:08:20 +0300

    feat(*): change folder structure

---
+++ app/src/main/java/com/example/memo/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.memo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
